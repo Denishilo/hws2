a176decd46b443d3bff9dd7ec18fb4c3af49ea86
Denishilo <denisanishenko@gmail.com>
2022-10-03 14:04:20 +0300

    fix 3 task with css

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -53,7 +53,7 @@ const Greeting: React.FC<GreetingPropsType> = (
                     className={s.button}
                     disabled={!name.trim()}
                 >
-                    add
+                    Add
                 </button>
             </div>
 
